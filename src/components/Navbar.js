0d07de9f536abb2d47a72b97a184326672a24a7f
901786 Cascino Romain <romain.cascino@gmail.com>
2018-09-22 11:12:06 +0700

    Make navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,48 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Navbar = () => (
+const Navbar = () => {
+
+  const toggle = () => {
+    document.querySelector(".burger").classList.toggle("is-active");
+    document.querySelector(".navbar-menu").classList.toggle("is-active");
+  }
+
+  return (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" style={{fontSize: "24px", color: "darkorange"}}>
           Kerinci Kultur
         </Link>
+        <span onClick={toggle} class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/eco-tours">
-          Eco Tours
-        </Link>
-        <Link className="navbar-item" to="/homestay">
-          Homestay
-        </Link>
-        <Link className="navbar-item" to="/gallery">
-          Gallery
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-      </div>
-      <div className="navbar-end">
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item" to="/eco-tours">
+            Eco Tours
+          </Link>
+          <Link className="navbar-item" to="/homestay">
+            Homestay
+          </Link>
+          <Link className="navbar-item" to="/gallery">
+            Gallery
+          </Link>
+          <Link className="navbar-item" to="/contact">
+            Contact
+          </Link>
+        </div>
       </div>
     </div>
   </nav>
-)
+  )
+}
 
 export default Navbar
