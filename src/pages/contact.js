69a2d111d79be5e693f8c47f6ab49cdb04718e06
901786 Cascino Romain <romain.cascino@gmail.com>
2018-09-24 15:20:09 +0700

    Add contact page with netlify form

---
+++ src/pages/contact.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+
+import Header from '../components/Header'
+
+export default class ContactPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Header image={this.props.data.mountKerinci.childImageSharp.fluid}/>
+        <section className="section">
+        <div className="container content" style={{maxWidth: '800px'}}>
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2">Contact</h1>
+          </div>
+          <form name="contact" method="POST" netlify>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input className="input" type="text"  name="name"/>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input className="input" type="email" name="email"/>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Subject</label>
+              <div className="control">
+                <input className="input" type="email" name="subject"/>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Message</label>
+              <div className="control">
+                <textarea className="textarea" name="message"></textarea>
+              </div>
+            </div>
+            <div className="control">
+              <button type="submit" className="button is-link">Submit</button>
+            </div>
+          </form>
+        </div>
+        </section>
+      </Layout>
+    )
+  }
+}
+
+export const query = graphql`
+  query {
+    mountKerinci: file(relativePath: {eq: "home.jpg"}) {
+      childImageSharp {
+        fluid(maxHeight: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+      }
+    }
+  }
+`
